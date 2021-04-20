185209a03f08ad87f0c79987ed8d0eac3f6b89bc
João Victor <joaovictorfrancorosa@gmail.com>
2021-04-19 21:25:13 -0300

    Projeto Inicial

---
+++ src/test/java/com/github/jvfranco/ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jvfranco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
